38b142e165551dead30e7ea2c35a4b57f65fdf27
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-11 20:50:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Question Number: 08
+// you should create four lines that look like this.
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing on each line.
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
